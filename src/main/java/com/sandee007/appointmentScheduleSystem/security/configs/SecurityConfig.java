5a200c2d878fa5fd2d10328f8093bb23e7b0ab28
Sandee007 <sandeepaalahakoon@gmail.com>
2023-08-07 21:48:33 +0530

    security config

---
+++ src/main/java/com/sandee007/appointmentScheduleSystem/security/configs/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.sandee007.appointmentScheduleSystem.security.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(config ->
+                                               config
+                                                       .requestMatchers("/").permitAll()
+                                                       .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+
+}

78f5339b2ae2ba7bcb87d847fb9899668acad2cb
Sandee007 <sandeepaalahakoon@gmail.com>
2023-08-07 19:09:31 +0530

    init

---
+++ src/main/java/com/sandee007/appointmentScheduleSystem/AppointmentScheduleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sandee007.appointmentScheduleSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentScheduleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentScheduleSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandee007/appointmentScheduleSystem/AppointmentScheduleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandee007.appointmentScheduleSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentScheduleSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
